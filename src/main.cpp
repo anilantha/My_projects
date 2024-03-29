cd54c2ae57cbec59933a68d016d9f8f273ade6f6
Anilanth <135984110+Anilanth@users.noreply.github.com>
2024-03-29 16:02:08 +0200

    teststststs

--- src/main.cpp
+++ src/main.cpp
@@ -39,5 +39,5 @@ void loop() {
     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(5000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);   
+  delay(1000);   
 }
